5d8e25c7377b6902d93799a5bbe1005ac241efee
hoshiyomiX <katahiranaruse.daydream@gmail.com>
2024-01-17 04:31:21 +0800

    Update and rename vendor to vendor.mk

---
+++ vendor.mk
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+  <!-- Vendor -->
+  <project name="excaliburXD/android_vendor_infinix_x695c"
+           path="vendor/infinix/note10pro"
+           remote="github"
+           revision="main-x695c" />
+  <!-- Extra vendor blob's -->
+  <project name="excaliburXD/extract_proprietary_blobs"
+           path="vendor/infinix/note10pro-blobs"
+           remote="github"
+           revision="main" />
+</manifest>
